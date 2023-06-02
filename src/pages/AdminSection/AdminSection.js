967f7fba6ddf8eb7ebe2b7c6ca145ccbdc1dace4
crCoelhosm <cristophersaar@gmail.com>
2023-06-02 14:15:55 -0500

    rgcli adequation on demand

---
+++ src/pages/AdminSection/AdminSection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './AdminSection.module.css';
+
+const AdminSection = () => (
+  <div className={styles.AdminSection}>
+    AdminSection Component
+  </div>
+);
+
+AdminSection.propTypes = {};
+
+AdminSection.defaultProps = {};
+
+export default AdminSection;
