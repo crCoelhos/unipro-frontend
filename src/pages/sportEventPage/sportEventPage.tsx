9b7a8c9a86343e63ce2327bbbcfd6bdb51819eb8
crCoelhosm <cristophersaar@gmail.com>
2023-06-13 19:56:18 -0500

    update(event creation) - new create button on event listing

--- src/pages/sportEventPage/sportEventPage.tsx
+++ src/pages/sportEventPage/sportEventPage.tsx
@@ -5,20 +5,28 @@ import HomeFooter from "../../components/homeFooter/homeFooter";
 import UpperNavbar from "../../components/Menu/Menu";
 import HomeComposedFooter from "../../components/homeComposedFooter/homeComposedFooter";
 import CreateEventModal from "../../components/CreateEventModal/CreateEventModal";
+import useLoginController from "../../controllers/LoginController";
 
 interface SportEventPageProps {}
 
-const SportEventPage: FC<SportEventPageProps> = () => (
-  <div className={styles.SportEventPage}>
-    <UpperNavbar />
-    <span>
-      <CreateEventModal />
-      <h1>Esportes</h1>
-    </span>
-    <hr />
-    <EventCatalogue />
-    <HomeComposedFooter />
-  </div>
-);
+const SportEventPage: FC<SportEventPageProps> = () => {
+  const { getSessionUser } = useLoginController();
+  const user = getSessionUser();
+  console.log("leticia: ", user);
 
+  const isAdmin = user?.role === 'ADMIN'; // Verifica se o usuário existe e se o user.role é 'ADMIN' -- o react não deixa fazer isso dentro do rendering
+
+  return (
+    <div className={styles.SportEventPage}>
+      <UpperNavbar />
+      <span>
+        {isAdmin && <CreateEventModal />}
+        <h1>Esportes</h1>
+      </span>
+      <hr />
+      <EventCatalogue />
+      <HomeComposedFooter />
+    </div>
+  );
+};
 export default SportEventPage;
