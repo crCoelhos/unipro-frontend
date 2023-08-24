dd4a2704b349364636acd7138b55fb10f45b60f5
crCoelhosm <cristophersaar@gmail.com>
2023-08-24 13:51:15 -0500

    update(pre payment) - pdf options

--- src/components/UseTermArea/UseTermArea.tsx
+++ src/components/UseTermArea/UseTermArea.tsx
@@ -1,8 +1,7 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Button } from 'react-bootstrap';
-import { UseTermAreaProps } from '../../types';
-import styles from './UseTermArea.module.css'
-
+import React, { useState, useEffect, useRef } from "react";
+import { Button } from "react-bootstrap";
+import { UseTermAreaProps } from "../../types";
+import styles from "./UseTermArea.module.css";
 
 const UseTermArea: React.FC<UseTermAreaProps> = ({ pdfFile }) => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -11,7 +10,8 @@ const UseTermArea: React.FC<UseTermAreaProps> = ({ pdfFile }) => {
 
   useEffect(() => {
     if (embedRef.current) {
-      const numPages = embedRef.current?.contentDocument?.querySelectorAll('.page').length;
+      const numPages =
+        embedRef.current?.contentDocument?.querySelectorAll(".page").length;
       setNumPages(numPages || null);
     }
   }, [pdfFile]);
@@ -30,11 +30,11 @@ const UseTermArea: React.FC<UseTermAreaProps> = ({ pdfFile }) => {
 
   return (
     <div className={styles.UseTermAreaContainer}>
-      <div className={styles.ButtonsContainer}>
-        <Button onClick={handlePreviousPage}>Anterior</Button>
-        {/* <span>{currentPage}</span> de <span>{numPages}</span> */}
-        <Button onClick={handleNextPage}>Seguinte</Button>
-      </div>
+      {/* <div className={styles.ButtonsContainer}> */}
+      {/* <Button onClick={handlePreviousPage}>Anterior</Button> */}
+      {/* <span>{currentPage}</span> de <span>{numPages}</span> */}
+      {/* <Button onClick={handleNextPage}>Seguinte</Button> */}
+      {/* </div> */}
       <div className={styles.UseTermArea}>
         <object
           data={pdfFile}
