0acd46349b2c426875dcc8857ccacc27df7fcfd7
ppaalo <60149992+ppaalo@users.noreply.github.com>
2023-08-26 22:28:13 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,10 +5,10 @@ REPO_FOLDER=/home/front/htdocs/uniproducoes.com.br
 
 cd $REPO_FOLDER
 
-git pull origin master
+git_pull_output=$(git pull origin master)
 
-if git rev-parse HEAD..origin/master >/dev/null 2>&1; then
-  echo "Nova atualização na main. Executando comando..."
+if [[ "$git_pull_output" != *"Already up to date."* ]]; then
+  echo "Nova atualização na master Executando comando..."
   
   npm install
   npm run build
@@ -19,5 +19,5 @@ if git rev-parse HEAD..origin/master >/dev/null 2>&1; then
   echo "Executado em: $current_date" >> /home/front/script_log.txt
 else
   current_date=$(date +"-%d-%m-%Y %H:%M:%S")
-  echo "$current_date Não há novas atualizações na main" >> /home/front/script_log.txt
+  echo "$current_date Não há novas atualizações na master" >> /home/front/script_log.txt
 fi
