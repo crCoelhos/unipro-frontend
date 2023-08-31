a9a43c9c6b11e5b974d65538e0c2c95289bb863d
crCoelhosm <cristophersaar@gmail.com>
2023-08-31 17:08:17 -0500

    update(athletic logo image) - style tweak

--- src/components/AthleticList/AthleticList.tsx
+++ src/components/AthleticList/AthleticList.tsx
@@ -101,7 +101,7 @@ const AthleticList: FC = () => {
                 </td>
                 <td>
                   {athletic.img_url && (
-                    <img src={`${url}uploads/athletics/${athletic.img_url}`} alt={athletic.name} />
+                    <img src={`${url}uploads/athletics/${athletic.img_url}`} alt={athletic.name} className={styles.AthleticLogoImg}/>
                   )}
                 </td>
                 <td>
